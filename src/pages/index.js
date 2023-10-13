fb0a88869a86ecd73326a5591a73dbb2f060105b
Fei Li <feili4287@gmail.com>
2023-10-12 20:34:28 -0400

    jsx excise

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Gallery from "./components/gallery";
+import Profilemis from "./qcomps/profile_mistake";
+import Firstcomp from "./qcomps/firstcomp";
+import Bio from "./qcomps/bios";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+      <Bio />
     </div>
-  )
+  );
 }
